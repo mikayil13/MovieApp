bded5a4cc018d6104a0baaac2399ccfd3f3580aa
mikayil13 <mikayilmemmedli9@gmail.com>
2025-02-15 18:22:36 +0400

    Contoller
    - TabBar
    -search,Home controller add
    - collection view add

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Mikayil on 15.02.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
